70b06f73460fd6d4c48907a6763add25b1e32516
Sebastian Amaya <88689185+saamayac@users.noreply.github.com>
2022-08-31 16:16:48 -0500

    Add files via upload

---
+++ lab1/README.md
@@ -0,0 +1,21 @@
+# Genral information
+
+## Name
+
+Sebatian Adair Amaya Ceballos
+
+## Operating system version
+
+Microsoft Windows 11 version 21H2
+
+## GHC version
+
+GHCVersion: The Glorious Glasgow Haskell Compilation System, version 9.2.4
+
+## alex version
+
+    3.2.7.1
+
+## happy version
+
+    1.20.0
---
+++ lab1/src/Syntax.hs
@@ -0,0 +1,17 @@
+
+module Syntax ( Term(..), Token(..) ) where
+
+data Term = Var String
+            | Lam String Term
+            | App Term Term
+        deriving Show
+
+-- Tokens use by the lexer and the parser.
+data Token = TokenDot
+            | TokenLambda
+            | TokenLParen
+            | TokenSemiColon
+            | TokenRParen
+            | TokenVar String
+        deriving Show
+
